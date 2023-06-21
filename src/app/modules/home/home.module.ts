9964201e8e2ef8d7bda890ca043b9d28555edd02
marianaivs <135011427+marianaivs@users.noreply.github.com>
2023-06-21 16:40:14 +0100

    1 file modified

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { SectionHeroComponent } from './sections/section-hero/section-hero.component';
+
 import { PageHomeComponent } from './pages/page-home/page-home.component';
 import { PageAboutComponent } from './pages/page-about/page-about.component';
-import { SectionFeaturesComponent } from './sections/section-features/section-features.component';
+
 import { ShowCarouselComponent } from './components/show-carousel/show-carousel.component';
 import { ShowSpotlightComponent } from './components/show-spotlight/show-spotlight.component';
+import { SectionFeaturesComponent } from './components/section-features/section-features.component';
+import { SectionHeroComponent } from './components/section-hero/section-hero.component';
 
 const COMPONENTS = [
   HomeComponent,
