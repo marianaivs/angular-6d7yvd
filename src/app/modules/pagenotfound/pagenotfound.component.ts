b2744722a740c8c2391ecd7840143148304e079c
marianaivs <135011427+marianaivs@users.noreply.github.com>
2023-06-21 16:37:51 +0100

    1 file modified, 6 added, 8 deleted

--- src/app/modules/pagenotfound/pagenotfound.component.ts
+++ src/app/modules/pagenotfound/pagenotfound.component.ts
@@ -2,8 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'pagenotfound',
-  templateUrl: './pagenotfound.component.html',
-  styleUrls: ['./pagenotfound.component.scss'],
+  template: `<div class="flex flex-col h-screen justify-center text-center">
+  <h1 class="font-bold text-2xl text-primary lg:text-3xl">404 Error</h1>
+  <h2 class="font-semibold mb-4 text-xl text-primary">Page Not Found</h2>
+  <p class="text-base text-gray-900 lg:text-xl">Where have all the pages gone? 👀</p>
+</div>
+`,
+  styles: [
+    `
+    :host {
+      display: flex;
+      flex-direction: column;
+    }
+    `,
+  ],
 })
 export class PageNotFoundComponent implements OnInit {
   constructor() {}
