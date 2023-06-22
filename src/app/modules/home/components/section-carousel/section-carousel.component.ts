1e542a446a14e38f8270f4a1b911b18b6b6ec3b1
marianaivs <135011427+marianaivs@users.noreply.github.com>
2023-06-22 12:21:34 +0100

    3 files modified

--- src/app/modules/home/components/section-carousel/section-carousel.component.ts
+++ src/app/modules/home/components/section-carousel/section-carousel.component.ts
@@ -1,14 +1,14 @@
 import { Component, Input } from '@angular/core';
-import { ShowService } from '../../../../shared/services/show.service';
 import { Show } from '../../../../core/interfaces/show.interface';
+import { ShowService } from '../../../../shared/services/show.service';
 
 @Component({
   selector: 'app-section-carousel',
   template: `
   <section class="bg-gray-100 py-60px">
   <div class="flex flex-nowrap flex-row justify-center gap-8">
-    <ng-container *ngIf="shows.length; else posterEmptyState">
-      <ng-template ngFor [ngForOf]="shows" let-show>
+   <ng-container *ngIf="show.length; else posterEmptyState">
+      <ng-template ngFor [ngForOf]="show" let-show>
         <app-show-poster></app-show-poster>
       </ng-template>
     </ng-container>
@@ -28,11 +28,11 @@ import { Show } from '../../../../core/interfaces/show.interface';
   ],
 })
 export class SectionCarouselComponent {
-  @Input() show!: Show;
+  show!: Show[];
 
-  constructor(private showService: ShowService) {
+  /* constructor(private showService: ShowService) {
     this.show = this.showService.getAllShows();
-  }
+  } */
 
   trackById(index: number, value: Show) {
     return value.id;
