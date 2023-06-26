1ca02a066fd1cb43108a26796e713890ec172098
marianaivs <135011427+marianaivs@users.noreply.github.com>
2023-06-26 09:08:35 +0100

    2 files modified, 1 added

--- src/app/modules/home/components/show-poster/show-poster.component.ts
+++ src/app/modules/home/components/show-poster/show-poster.component.ts
@@ -17,9 +17,7 @@ import { ShowService } from '../../../../shared/services/show.service';
 export class ShowPosterComponent {
   @Input() show!: Show;
 
-  constructor(private showService: ShowService) {
-    /* this.show = this.showService.getAllShows(); */
-  }
+  constructor(private showService: ShowService) {}
 
   trackById(index: number, value: Show) {
     return value.id;
--- src/app/shared/components/phone-mockup/phone-mockup.component.ts
+++ src/app/shared/components/phone-mockup/phone-mockup.component.ts
@@ -8,17 +8,7 @@ type PhoneMockupType = typeof PhoneMockupEnum[keyof typeof PhoneMockupEnum];
 
 @Component({
   selector: 'app-phone-mockup',
-  template: `
-    <div class="flex justify-center relative shrink-0 z-10">
-      <video *ngIf="getSrcType(src) && getSrcType(src) === phoneMockupEnum.Video" [src]="src" class="absolute max-h-[600px] top-0 w-auto z-50"></video>
-      <img
-        *ngIf="getSrcType(src) && getSrcType(src) === phoneMockupEnum.Image"
-        [src]="src"
-        class="absolute max-h-[600px] top-0 w-auto z-50"
-      />
-      <img src="https://raw.githubusercontent.com/marianaivs/angular-bktjab/main/assets/app-mockup.png" class="max-h-[600px] rounded-[50px]" />
-    </div>
-  `,
+  templateUrl: 'phone-mockup.component.html',
   styles: [
     `
       :host {
